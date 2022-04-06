c9c78b21c3f6dbc1c4a9f8038afb00157b7dda0a
efraintinoco <oakcliff_21@yahoo.com>
2022-04-05 21:39:05 -0500

    add links

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const url ="https://thesimpsonsquoteapi.glitch.me/quotes?count=50"
+const url = "https://thesimpsonsquoteapi.glitch.me/quotes?count=40"
 const main = document.querySelector("main")
 
 
-function setState(simpson) { 
+function simpsonCharacter(simpson) {
 
     const div = document.createElement("div")
     div.innerHTML = `
     <div>
     <h1>${simpson.character}:</h1>
     <p>"${simpson.quote}"</p>
+    <figure>
     <img src=${simpson.image} alt=${simpson.character} />
-    <button onClick=${simpson.callTheApi}>Next Character</button>
-  </div>
+    <figcaption><a href="simpsons.html?simpson=${simpson.character}">${simpson.character}</a></figcaption>
+    </figure>
+    
+    </div>
 `
-main.append(div)
+    main.append(div)
 }
 
-  
-        fetch(url)
-            .then(urlInfo => {
-                return urlInfo.json();
-            })
-            .then(jsonInfo => {
-                console.log(jsonInfo);
-                this.setState({
-                    quote: jsonInfo[0].quote,
-                    character: jsonInfo[0].character,
-                    image: jsonInfo[0].image,
-                    characterDirection: jsonInfo[0].characterDirection,
-                    prueba: true
-                });
-            });
 
- 
+fetch(url)
+    .then(urlInfo => {
+        return urlInfo.json();
+    })
+    .then(jsonInfo => {
+        jsonInfo.forEach(urlInfo => {
+            simpsonCharacter(urlInfo)
+
+
+        });
+    });
+
+
--- simpsons.js
+++ simpsons.js
@@ -2,37 +2,36 @@
 const main = document.querySelector("main")
 
 
-function setState(simpson) { 
+function simpsonCharacter(simpsons) {
 
     const div = document.createElement("div")
     div.innerHTML = `
     
     <div>
-    <h1>${simpson.character}:</h1>
-    <p>"${simpson.quote}"</p>
-    <img src=${simpson.image} alt=${simpson.character} <a href="simpsons.html?quote=${simpson.character}"> />
-    <button onClick=${simpson.callTheApi}>Next Character</button>
+    <h1>${simpsons.character}:</h1>
+    <p>"${simpsons.quote}"</p>
+    <figure>
+    <img src=${simpsons.image} alt=${simpsons.character} />
+    <figcaption><a href="simpsons.html?simpsons=${simpsons.character}">${simpsons.character}</a></figcaption>
+    </figure>
+    
+    
   </div>
 `
-main.append(div)
+    main.append(div)
 }
 
 
 const url = new URL(window.location)
 const queryString = new URLSearchParams(url.search)
-fetch(`https://thesimpsonsquoteapi.glitch.me/quotes${queryString.get('quote')}`)
-  
-        fetch(url)
-            .then(urlInfo => {
-                return urlInfo.json();
-            })
-            .then(jsonInfo => {
-                console.log(jsonInfo);
-                this.setState({
-                    quote: jsonInfo[0].quote,
-                    character: jsonInfo[0].character,
-                    image: jsonInfo[0].image,
-                    characterDirection: jsonInfo[0].characterDirection,
-                    prueba: true
-                });
-            });
+fetch(`https://thesimpsonsquoteapi.glitch.me/quotes${queryString.get('simpsons')}`)
+
+fetch(url)
+    .then(urlInfo => {
+        return urlInfo.json();
+    })
+    .then(jsonInfo => {
+        jsonInfo.forEach(urlInfo => {
+            simpsonCharacter(urlInfo)
+        });
+    });
