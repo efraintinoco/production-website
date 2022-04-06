0cf4bf59df70f646ac428dd7381780ac8c85f9ce
efraintinoco <oakcliff_21@yahoo.com>
2022-04-06 17:24:51 -0500

    add styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const url = "https://thesimpsonsquoteapi.glitch.me/quotes?count=100"
+const url = "https://thesimpsonsquoteapi.glitch.me/quotes?count=40"
 const main = document.querySelector("main")
 
 
@@ -7,8 +7,8 @@ function simpsonCharacter(simpson) {
     const div = document.createElement("div")
     div.innerHTML = `
     <div>
+    <h1>${simpson.character}:</h1>
     
-
     <figure>
     <img src=${simpson.image} alt=${simpson.character} />
     <figcaption><a href="simpsons.html?character=${simpson.character}">${simpson.character}</a></figcaption>
