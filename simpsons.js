2453d56a09f6815448aadce56fe8672b7e5568ec
efraintinoco <oakcliff_21@yahoo.com>
2022-04-20 20:27:34 -0500

    fix js

--- simpsons.js
+++ simpsons.js
@@ -1,6 +1,6 @@
 const main = document.querySelector('main')
 
-function simpsonCharacter (simpson) {
+function simpsonCharacter(simpson) {
   const div = document.createElement('div')
   div.innerHTML = `
     <div>
@@ -19,7 +19,6 @@ function simpsonCharacter (simpson) {
 const url = new URL(window.location)
 const queryString = new URLSearchParams(url.search)
 fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${queryString.get('character')}`)
-
   .then(urlInfo => {
     return urlInfo.json()
   })
